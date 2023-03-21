45f595bb7284312774d613fd3bd482be6fb2bf9a
Marcel-Kro <marcel-bastian.kromm@stud-hs-hannover.de>
2023-03-21 22:28:41 +0100

    Abgabe Übung 5

--- 05/uebung_05.js
+++ 05/uebung_05.js
@@ -1,43 +1,40 @@
 // Übung 5
-console.log("=== Übung 5 - jQuery ===");
-console.log();
+
 
 // Übung 5.1a
 // Finden Sie den div-Container über seine Klasse 'container01' und geben Sie an,
 // wie man mittels einer Methode auf die gesamte Breite eines Elements (inkl. padding, margin und border) zugreift bzw. herausfindet.
-console.log("== Übung 5.1a ==");
-
-console.log();
+$(document).ready(function(){
+    $('.container01').html('<b>5.1a:</b> '+$('.container01').outerWidth(true));
+});
 
 // Übung 5.1b
 // Wählen Sie die Checkbox über seine ID 'remember' aus und haken Sie diese über eine Methode an.
-console.log("== Übung 5.1b ==");
-
-console.log();
+$(document).ready(function(){
+    $('#remember').prop('checked', true);
+});
 
 // Übung 5.1c
 // Wählen Sie die h1-Überschriften über ihren Tag-Namen aus und ändern Sie die Schriftfarbe mittels einer Methode.
 // Verwenden Sie für die Angabe Ihrer Farbe einen Hex-Farbcode.
-console.log("== Übung 5.1c ==");
-
-console.log();
+$(document).ready(function(){
+    $('h1').css('color', '#0010FF');
+});
 
 // Übung 5.1d
 // Blenden Sie das (über CSS ausgeblendete) Bild mit der ID 'image' mit Hilfe einer Methode ein.
-console.log("== Übung 5.1d ==");
-
-console.log();
+$(document).ready(function(){
+    $('#image').show();
+});
 
 // Übung 5.1e
 // Löschen Sie mittels einer Methode alle Kind-Elemente (Child-Nodes) des div-Containers mit der Klasse 'container02'.
-console.log("== Übung 5.1e ==");
-
-console.log();
+$(document).ready(function(){
+    $('.container02').empty();
+});
 
 // 5.2 Advanced
-console.log();
-console.log("=== 5.2 - Advanced ===");
-console.log();
+
 
 // Variable
 var first = $('#first');
@@ -51,14 +48,49 @@ var btn = $('#addName');
 //      - eine neue Tabellenreihe mit zwei Tabellenzellen erzeugt werden, die die eingegebenen Werte aus den jeweiligen Eingabefelder 'Vorname' und 'Nachname' anzeigt.
 //      - die erzeugte Tabellenreihe zu der Tabelle hinzugefügt werden.
 //      - die beiden Eingabefelder Vor- und Nachname im Anschluss leeren.
-console.log("== Übung 5.2a ==");
 
-console.log();
+function addRow(){
+    var firstName = $('#first').val();
+    var lastName = $('#last').val();
+    var tableRow = '<tr><td>' + firstName + '</td><td>' + lastName + '</td></tr>';
+    if(firstName == '' || lastName == ''){
+        $('#warning').show();
+    } else {
+        $('#table tbody').append(tableRow);
+        $('#first').val('');
+        $('#last').val('');
+        $('#warning').hide();
+    }
+};
+$(document).ready(function(){
+    $('#addName').click(addRow);
+});
 
 // Übung 5.2b
 // Rufen Sie während der Eingabe bzw. dem Tippen innerhalb der Eingabefelder 'Vorname' und 'Nachname' die untenstehende Funktion 'checkEnterAndAdd' auf.
 // In der Funktion soll überprüft werden, ob die Enter-Taste gedrückt wurde.
 // Ist dies der Fall, soll die Funktion 'addRow' aus der vorherigen Aufgabe aufgerufen werden.
-console.log("== Übung 5.2b ==");
+function checkEnterAndAdd(event){
+    if (event.keyCode == 13){
+        addRow();
+    }
+}
+$(document).ready(function(){
+    $('#warning').hide();
+    $('#first').keyup(checkEnterAndAdd);
+    $('#last').keyup(checkEnterAndAdd);
+});
 
-console.log();
+
+// Extra
+// 1. Vor- und Nachname müssen eingetragen werden,
+// ansonsten wird Warnung gezeigt
+
+// 2. Reset der Tabelle
+$(document).ready(function(){
+    $('#warning').hide();
+    $('#removeTable').click(removeTable);
+});
+function removeTable(){
+    $('tbody').empty();
+};
